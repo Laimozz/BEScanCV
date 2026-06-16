6afe898a1bb5c07926093fc1b3243695599c09e3
TranNguyenPhuc <trannguyenphuc1902@gmail.com>
2026-06-16 13:44:48 +0700

    feat: implement CV filtering functionality by adding CvGetAllFilterDto and updating CvGetAllRequest and CvGetAllService

---
+++ BEScanCV.Application/DTOS/CvGetAllFilterDto.cs
@@ -0,0 +1,9 @@
+namespace BEScanCV.Application.DTOS;
+
+public class CvGetAllFilterDto
+{
+    public int? TotalExperienceYears { get; set; }
+    public string? Skills { get; set; }
+    public string? Position { get; set; }
+    // TODO: Add more filters
+}
--- BEScanCV.Application/DTOS/CvGetAllRequest.cs
+++ BEScanCV.Application/DTOS/CvGetAllRequest.cs
@@ -5,5 +5,5 @@ public class CvGetAllRequest
     public int Page { get; set; } = 1;
     public int Limit { get; set; } = 10;
     public string? Search { get; set; }
-    public string? Extensions { get; set; } // For future implementations e.g sort, filter,....
+    public CvGetAllFilterDto? Filter { get; set; }
 }
